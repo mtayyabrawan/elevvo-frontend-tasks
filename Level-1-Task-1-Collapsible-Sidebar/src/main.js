d35a9b22eaf72005e291a086e594b6fb78bca15d
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-09-24 21:56:59 +0500

    completed basic sidebar with collapsible functionality

---
+++ Level-1-Task-1-Collapsible-Sidebar/src/main.js
@@ -0,0 +1,21 @@
+const menuClose = document.querySelector(".menu-close");
+const menuOpen = document.querySelector(".menu-open");
+const sidebar = document.querySelector(".sidebar");
+const nav = document.querySelector("nav");
+const sidebarContent = document.querySelector(".sidebar-inner");
+
+menuOpen.addEventListener("click", () => {
+    nav.classList.toggle("nav-open");
+    sidebar.classList.toggle("sidebar-open");
+    setTimeout(() => {
+        sidebarContent.classList.toggle("hidden");
+    }, 600);
+});
+
+menuClose.addEventListener("click", () => {
+    sidebarContent.classList.toggle("hidden");
+    nav.classList.toggle("nav-open");
+    setTimeout(() => {
+        sidebar.classList.toggle("sidebar-open");
+    }, 600);
+});
