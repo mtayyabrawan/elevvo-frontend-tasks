27b36ae3836d497dda5fedfcea9cefc81544ad1d
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-09-26 11:38:45 +0500

    removed explicit dark class added theme toggle component

---
+++ Level-2-Task-1-TaskApp-Landing-Page/src/components/ui/primary/ThemeToggle.tsx
@@ -0,0 +1,56 @@
+import { IconDeviceDesktop, IconMoon, IconSun } from "@tabler/icons-react";
+import { useEffect, useState, type MouseEvent } from "react";
+
+function ThemeToggle() {
+    const [theme, setTheme] = useState<string>("");
+
+    useEffect(() => {
+        document.documentElement.classList.toggle(
+            "dark",
+            localStorage.taskeep_theme === "dark" ||
+                (!("taskeep_theme" in localStorage) &&
+                    window.matchMedia("(prefers-color-scheme: dark)").matches)
+        );
+        if ("taskeep_theme" in localStorage) {
+            setTheme(localStorage.taskeep_theme);
+        } else setTheme("system");
+    }, [theme]);
+
+    function toggleTheme(e: MouseEvent<HTMLButtonElement>) {
+        if (e.currentTarget.id === "system") {
+            localStorage.removeItem("taskeep_theme");
+            setTheme("system");
+        } else if (e.currentTarget.id === "light") {
+            localStorage.taskeep_theme = "light";
+            setTheme("light");
+        } else {
+            localStorage.taskeep_theme = "dark";
+            setTheme("dark");
+        }
+    }
+    const buttons = [
+        { name: "system", icon: <IconDeviceDesktop size={16} /> },
+        { name: "light", icon: <IconSun size={16} /> },
+        { name: "dark", icon: <IconMoon size={16} /> },
+    ];
+    return (
+        <div
+            id="theme"
+            className="flex cursor-pointer appearance-none items-center justify-center gap-0.75 rounded-2xl bg-stone-600 px-0.75 py-0.5 ring-[0.75px] focus-visible:outline-hidden"
+        >
+            {buttons.map((btn) => (
+                <button
+                    key={btn.name}
+                    id={btn.name}
+                    type="button"
+                    onClick={toggleTheme}
+                    className={`transile cursor-pointer rounded-full p-1 hover:bg-stone-500 focus-visible:outline-hidden ${theme === btn.name ? "bg-stone-800" : "bg-transparent"}`}
+                >
+                    {btn.icon}
+                </button>
+            ))}
+        </div>
+    );
+}
+
+export default ThemeToggle;
