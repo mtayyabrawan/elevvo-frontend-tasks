7779d9c2b45d88038e7bd4dae0ee5f94f5fa7bdb
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-09-26 11:43:39 +0500

    added copyright section in footer

--- Level-2-Task-1-TaskApp-Landing-Page/src/components/ui/Layout.tsx
+++ Level-2-Task-1-TaskApp-Landing-Page/src/components/ui/Layout.tsx
@@ -7,7 +7,7 @@ function Layout() {
         <div className="bg-radial-[at_50%_65%] from-neutral-100 from-0% to-neutral-400 to-100% text-neutral-800 dark:from-stone-800 dark:to-stone-700 dark:text-neutral-300">
             <main className="relative mx-auto max-w-[1540px] min-w-[320px] p-1">
                 <Navbar />
-                <main className="mt-3.5 min-h-[calc(100vh-16.875rem)]">
+                <main className="mt-3.5 min-h-[calc(100vh-23.875rem)]">
                     <Outlet />
                 </main>
                 <Footer />
--- Level-2-Task-1-TaskApp-Landing-Page/src/components/ui/primary/Footer.tsx
+++ Level-2-Task-1-TaskApp-Landing-Page/src/components/ui/primary/Footer.tsx
@@ -1,9 +1,10 @@
 import NewsLetter from "../../NewsLetter";
+import ThemeToggle from "./ThemeToggle";
 
 function Footer() {
     return (
-        <footer className="mx-auto grid h-52 w-full grid-cols-2 grid-rows-3 rounded-lg bg-stone-800 shadow-[0px_0px_5px_1px_black_inset] ring-[0.25px] ring-neutral-100">
-            <div className="flex w-full items-center justify-between px-8">
+        <footer className="mx-auto grid h-80 w-full grid-cols-2 grid-rows-4 rounded-lg bg-stone-800 shadow-[0px_0px_5px_1px_black_inset] ring-[0.25px] ring-neutral-100">
+            <div className="flex h-full w-full items-center justify-between px-8">
                 <div className="text-sm">
                     <h1 className="text-lg font-medium">Newsletter</h1>
                     <p className="mt-1 text-sm">
@@ -13,8 +14,16 @@ function Footer() {
                 <NewsLetter />
             </div>
             <div></div>
-            <div className="col-span-2"></div>
-            <div className="col-span-2"></div>
+            <div className="col-span-2 row-start-2 row-end-4 h-full w-full"></div>
+            <div className="col-span-2 flex h-full w-full items-center justify-between px-8 text-xs">
+                <div>
+                    <ThemeToggle />
+                </div>
+                <p>
+                    Copyright &copy; {new Date().getFullYear()} | TasKeep.com |
+                    All rights reserved
+                </p>
+            </div>
         </footer>
     );
 }
