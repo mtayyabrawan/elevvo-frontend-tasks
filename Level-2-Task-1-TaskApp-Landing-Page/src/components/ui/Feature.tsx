460d582a486942226e8a767d56c158c4d5952bb9
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-09-27 20:10:23 +0500

    added reuseable feature card for displaying diff features

---
+++ Level-2-Task-1-TaskApp-Landing-Page/src/components/ui/Feature.tsx
@@ -0,0 +1,47 @@
+import { type ReactNode } from "@tabler/icons-react";
+
+function Feature({
+    icon,
+    title,
+    description,
+    imageSrc,
+    position,
+}: {
+    icon: ReactNode;
+    title: string;
+    description: string;
+    imageSrc: string;
+    position: "left" | "right";
+}) {
+    {
+        const positionClasses = {
+            left: "flex-row",
+            right: "flex-row-reverse",
+        };
+        return (
+            <div
+                className={`mx-auto flex w-[90%] ${positionClasses[position]} items-center justify-between`}
+            >
+                <div className="h-60 w-[47%]">
+                    <img
+                        src={imageSrc}
+                        alt="image"
+                        className="h-full w-full object-cover"
+                    />
+                </div>
+                <div className="h-40 w-[0.5px] bg-white"></div>
+                <div className="flex h-60 w-[47%] flex-col gap-3 p-4">
+                    <h3 className="flex items-center justify-start gap-5">
+                        {icon}
+                        <p className="font-medium">{title}</p>
+                    </h3>
+                    <p className="line-clamp-6 text-justify indent-10 text-xs leading-loose text-pretty">
+                        {description}
+                    </p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Feature;
