21d05858f89e4da3fac8361ce88aa10f051d04dc
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-09-30 19:02:23 +0500

    created navbar responsive

--- Level-2-Task-1-TaskApp-Landing-Page/src/components/ui/Layout.tsx
+++ Level-2-Task-1-TaskApp-Landing-Page/src/components/ui/Layout.tsx
@@ -4,10 +4,10 @@ import Footer from "./primary/Footer";
 
 function Layout() {
     return (
-        <div className="bg-radial-[at_50%_65%] from-neutral-100 from-0% to-neutral-400 to-100% text-neutral-800 dark:from-stone-800 dark:to-stone-700 dark:text-neutral-300">
-            <main className="relative mx-auto max-w-[1540px] min-w-[320px] p-1">
+        <div className="w-full bg-radial-[at_50%_65%] from-neutral-100 from-0% to-neutral-400 to-100% text-neutral-800 dark:from-stone-800 dark:to-stone-700 dark:text-neutral-300">
+            <main className="relative mx-auto w-full max-w-[1540px] min-w-[320px] p-0.5 sm:p-1">
                 <Navbar />
-                <main className="mt-3.5 min-h-[calc(100vh-23.875rem)]">
+                <main className="mt-3.5 min-h-[calc(100vh-23.875rem)] w-full">
                     <Outlet />
                 </main>
                 <Footer />
--- Level-2-Task-1-TaskApp-Landing-Page/src/components/ui/primary/Navbar.tsx
+++ Level-2-Task-1-TaskApp-Landing-Page/src/components/ui/primary/Navbar.tsx
@@ -9,8 +9,8 @@ function Navbar() {
         { lable: "Blog", link: "#blog" },
     ];
     return (
-        <header className="sticky top-3 z-10 mx-auto grid h-12 w-[90%] grid-cols-2 grid-rows-1 place-items-center rounded-full bg-stone-50/40 px-1.5 ring-[1px] ring-stone-100 drop-shadow-sm drop-shadow-stone-200 backdrop-blur-xs dark:bg-stone-600/40 dark:ring-stone-500 dark:drop-shadow-stone-900">
-            <div className="flex h-full w-full items-center justify-start pl-4 text-lg font-medium">
+        <header className="sticky top-1 z-10 mx-auto grid h-10 grid-cols-2 grid-rows-1 place-items-center rounded-full bg-stone-50/40 px-1.5 ring-[1px] ring-stone-100 drop-shadow-sm drop-shadow-stone-200 backdrop-blur-xs sm:top-3 sm:h-12 sm:w-[90%] dark:bg-stone-600/40 dark:ring-stone-500 dark:drop-shadow-stone-900">
+            <div className="flex h-full w-full items-center justify-start text-sm font-medium sm:pl-4 sm:text-lg">
                 <Link
                     to="/"
                     className="tracking-wider text-shadow-[0px_0px_5px_var(--color-stone-400)] dark:text-shadow-[0px_0px_5px_var(--color-stone-900)]"
@@ -18,7 +18,7 @@ function Navbar() {
                     TasKeep
                 </Link>
             </div>
-            <nav className="flex h-[80%] w-fit items-center justify-center gap-7 justify-self-end rounded-full bg-stone-400/60 px-[1rem] text-sm dark:bg-stone-800/60">
+            <nav className="flex h-[80%] w-fit items-center justify-center gap-2 justify-self-end rounded-full bg-stone-400/60 px-2 text-[0.65rem] sm:gap-7 sm:px-4 sm:text-sm dark:bg-stone-800/60">
                 {navlinks.map((link, idx) => (
                     <a
                         key={idx}
