258e38eee454d02c8ce399134396d5099745f705
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-10-01 10:31:31 +0500

    added hero section image

--- Level-2-Task-1-TaskApp-Landing-Page/src/components/ui/HeroSection.tsx
+++ Level-2-Task-1-TaskApp-Landing-Page/src/components/ui/HeroSection.tsx
@@ -2,13 +2,13 @@ import { IconBasketCheck } from "@tabler/icons-react";
 
 function HeroSection() {
     return (
-        <div className="flex h-60 w-full items-center justify-center">
-            <div className="w-1/2 space-y-5">
+        <div className="flex h-60 w-full items-center justify-center rounded-md bg-[url(/hero.png)] bg-cover bg-center">
+            <div className="flex h-full w-full flex-col items-start justify-center gap-4 rounded-md bg-stone-50/20 pl-28 backdrop-blur-[2px] dark:bg-stone-500/60">
                 <div className="flex items-center justify-start gap-6 px-4">
                     <IconBasketCheck size={50} />
                     <h1 className="text-2xl font-medium">TasKeep</h1>
                 </div>
-                <p className="w-1/2 text-sm leading-loose font-thin">
+                <p className="w-1/4 text-sm leading-loose font-thin">
                     A next generation Task Tracker app where you can keep track
                     of all your tasks and stay upto date.
                 </p>
