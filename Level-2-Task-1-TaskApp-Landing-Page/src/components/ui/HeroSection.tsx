1f7db8ac39a48f3737ae057e079274ec8dcf08ba
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-10-01 14:59:00 +0500

    made hero section responsive

--- Level-2-Task-1-TaskApp-Landing-Page/src/components/ui/HeroSection.tsx
+++ Level-2-Task-1-TaskApp-Landing-Page/src/components/ui/HeroSection.tsx
@@ -2,19 +2,21 @@ import { IconBasketCheck } from "@tabler/icons-react";
 
 function HeroSection() {
     return (
-        <div className="flex h-60 w-full items-center justify-center rounded-md bg-[url(/hero.png)] bg-cover bg-center">
-            <div className="flex h-full w-full flex-col items-start justify-center gap-4 rounded-md bg-stone-50/20 pl-28 backdrop-blur-[2px] dark:bg-stone-500/60">
-                <div className="flex items-center justify-start gap-6 px-4">
-                    <IconBasketCheck size={50} />
-                    <h1 className="text-2xl font-medium">TasKeep</h1>
+        <div className="flex h-28 w-full items-center justify-center rounded-md bg-[url(/hero.png)] bg-cover bg-center sm:h-48 md:h-60">
+            <div className="flex h-full w-full flex-col items-start justify-center gap-1 rounded-md bg-stone-50/20 pl-4 backdrop-blur-[2px] sm:gap-1.5 sm:pl-14 md:gap-4 md:pl-20 lg:pl-28 dark:bg-stone-500/60">
+                <div className="flex items-center justify-start gap-4 px-1 sm:px-4 md:gap-6">
+                    <IconBasketCheck className="size-8 sm:size-10 md:size-14" />
+                    <h1 className="text-lg font-medium sm:text-xl md:text-2xl">
+                        TasKeep
+                    </h1>
                 </div>
-                <p className="w-1/4 text-sm leading-loose font-thin">
+                <p className="w-[95%] text-xs leading-normal font-thin sm:w-[80%] sm:text-sm sm:leading-loose md:w-[50%] lg:w-[40%]">
                     A next generation Task Tracker app where you can keep track
                     of all your tasks and stay upto date.
                 </p>
                 <a
                     href="#get-started"
-                    className="transile ml-4 w-fit rounded-2xl bg-neutral-400 px-3 py-1.5 text-sm font-medium hover:bg-neutral-600/50 dark:bg-neutral-900 dark:hover:bg-neutral-900/50"
+                    className="transile ml-2 w-fit rounded-2xl bg-neutral-400 px-2 py-1 text-[0.55rem] font-medium hover:bg-neutral-600/50 sm:ml-4 sm:px-3 sm:py-1.5 sm:text-xs md:text-sm dark:bg-neutral-900 dark:hover:bg-neutral-900/50"
                 >
                     Get Started
                 </a>
