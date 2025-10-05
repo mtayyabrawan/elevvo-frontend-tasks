bf7ce4aec80163926522d539af92c93ef6dc80fb
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-10-05 22:08:46 +0500

    added faqs to contact page

---
+++ Level-3-Task-1-Freelance-Client-Dashboard/src/components/ui/FAQ.tsx
@@ -0,0 +1,56 @@
+function FAQ() {
+    return (
+        <div className="h-auto w-full space-y-5">
+            <h2 className="text-center text-lg font-medium">FAQ's</h2>
+            <div className="mx-auto h-auto w-[60%] space-y-2 rounded-md bg-slate-400/40 p-2 shadow-[0px_0px_4px_-1px_black]">
+                <details className="w-full">
+                    <summary className="transile cursor-pointer rounded-md bg-slate-400 p-2 text-base font-medium hover:bg-slate-400/80 focus-visible:outline-hidden">
+                        Where to report error?
+                    </summary>
+                    <p className="p-4 text-sm leading-loose">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Architecto vitae, voluptas repudiandae reiciendis
+                        at fuga. Quos, itaque incidunt facilis voluptatibus
+                        consequatur rem facere exercitationem et blanditiis quo
+                        tenetur vero quidem eius consequuntur molestias
+                        consectetur, nam ad aperiam suscipit architecto, magni
+                        sapiente sunt. Iure temporibus adipisci mollitia fugit
+                        itaque qui ab?
+                    </p>
+                </details>
+                <details className="w-full">
+                    <summary className="transile cursor-pointer rounded-md bg-slate-400 p-2 text-base font-medium hover:bg-slate-400/80 focus-visible:outline-hidden">
+                        Do you support international call?
+                    </summary>
+                    <p className="p-4 text-sm leading-loose">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Architecto vitae, voluptas repudiandae reiciendis
+                        at fuga. Quos, itaque incidunt facilis voluptatibus
+                        consequatur rem facere exercitationem et blanditiis quo
+                        tenetur vero quidem eius consequuntur molestias
+                        consectetur, nam ad aperiam suscipit architecto, magni
+                        sapiente sunt. Iure temporibus adipisci mollitia fugit
+                        itaque qui ab?
+                    </p>
+                </details>
+                <details className="w-full">
+                    <summary className="transile cursor-pointer rounded-md bg-slate-400 p-2 text-base font-medium hover:bg-slate-400/80 focus-visible:outline-hidden">
+                        How much time you take to reply?
+                    </summary>
+                    <p className="p-4 text-sm leading-loose">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Architecto vitae, voluptas repudiandae reiciendis
+                        at fuga. Quos, itaque incidunt facilis voluptatibus
+                        consequatur rem facere exercitationem et blanditiis quo
+                        tenetur vero quidem eius consequuntur molestias
+                        consectetur, nam ad aperiam suscipit architecto, magni
+                        sapiente sunt. Iure temporibus adipisci mollitia fugit
+                        itaque qui ab?
+                    </p>
+                </details>
+            </div>
+        </div>
+    );
+}
+
+export default FAQ;
--- Level-3-Task-1-Freelance-Client-Dashboard/src/pages/Contact.tsx
+++ Level-3-Task-1-Freelance-Client-Dashboard/src/pages/Contact.tsx
@@ -1,10 +1,11 @@
 import ContactForm from "../components/ui/ContactForm";
 import ContactInfo from "../components/ui/ContactInfo";
+import FAQ from "../components/ui/FAQ";
 
 function Contact() {
     return (
-        <div className="w-full space-y-4 p-2">
-            <div className="flex h-96 w-full items-center rounded-sm bg-slate-400 bg-[url(/wallpaper-2.jpg)] bg-cover bg-center shadow-[0px_0px_2px_1px_black]">
+        <div className="w-full space-y-6 p-2 pb-6">
+            <div className="flex h-96 w-full items-center rounded-sm bg-[url(/wallpaper-2.jpg)] bg-cover bg-center shadow-[0px_0px_2px_1px_black]">
                 <div className="ml-56 flex w-fit flex-col items-start justify-center gap-4 rounded-md bg-slate-100/20 p-8 shadow-[0px_0px_5px_1px_black] backdrop-blur-xs">
                     <h1 className="text-xl font-medium">Contact Us</h1>
                     <p className="w-[30rem] text-justify indent-1 text-sm leading-loose text-pretty">
@@ -18,6 +19,7 @@ function Contact() {
                 <ContactInfo />
                 <ContactForm />
             </div>
+            <FAQ />
         </div>
     );
 }
