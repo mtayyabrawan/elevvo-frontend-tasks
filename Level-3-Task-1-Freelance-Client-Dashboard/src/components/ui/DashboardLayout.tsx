71d79e25752c148d942c54f607582f2bb2527507
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-10-08 22:50:13 +0500

    added conditional rendring in dashboard layout

--- Level-3-Task-1-Freelance-Client-Dashboard/src/components/ui/DashboardLayout.tsx
+++ Level-3-Task-1-Freelance-Client-Dashboard/src/components/ui/DashboardLayout.tsx
@@ -1,20 +1,29 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import BaseLayout from "./common/BaseLayout";
+import useAuth from "../../hooks/useAuth";
+import Loader from "./Loader";
 
 function DashboardLayout() {
-    return (
-        <BaseLayout height="screen">
-            <Header />
-            <div className="flex h-[calc(100vh-3rem)] w-full">
-                <Sidebar />
-                <main className="scrollbar-custom h-full w-[85%] overflow-y-auto">
-                    <Outlet />
-                </main>
-            </div>
-        </BaseLayout>
-    );
+    const navigator = useNavigate();
+    const { loggedIn } = useAuth();
+    if (loggedIn === true)
+        return (
+            <BaseLayout height="screen">
+                <Header />
+                <div className="flex h-[calc(100vh-3rem)] w-full">
+                    <Sidebar />
+                    <main className="scrollbar-custom h-full w-[85%] overflow-y-auto">
+                        <Outlet />
+                    </main>
+                </div>
+            </BaseLayout>
+        );
+    else if (loggedIn === "pending") return <Loader />;
+    else {
+        navigator("/");
+    }
 }
 
 export default DashboardLayout;
---
+++ Level-3-Task-1-Freelance-Client-Dashboard/src/components/ui/Loader.tsx
@@ -0,0 +1,9 @@
+function Loader() {
+    return (
+        <div className="fixed top-0 left-1/2 flex h-full w-full max-w-[1540px] -translate-x-1/2 items-center justify-center bg-zinc-800 text-white">
+            <h1>Loading...</h1>
+        </div>
+    );
+}
+
+export default Loader;
