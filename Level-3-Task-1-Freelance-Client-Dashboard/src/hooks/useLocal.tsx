81e2f4e704f232c6935d424f76b7c50c0041845e
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-10-09 09:36:30 +0500

    added custom hook for local storage

---
+++ Level-3-Task-1-Freelance-Client-Dashboard/src/hooks/useLocal.tsx
@@ -0,0 +1,21 @@
+function useLocal() {
+    function setLocal<T>({ key, value }: { key: string; value: T }): void {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+    
+    function getLocal<T>(key: string): T | null {
+        const localData = localStorage.getItem(key);
+        if (!localData) {
+            return null;
+        }
+        const parsedData = JSON.parse(localData!) as T;
+        return parsedData;
+    }
+
+    function removeLocal(key: string): void {
+        localStorage.removeItem(key);
+    }
+    return { setLocal, getLocal, removeLocal };
+}
+
+export default useLocal;
