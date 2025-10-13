94e4081e616f06a47213eb5df97bf4425f3119f4
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-10-13 18:59:50 +0500

    added hasklink for navigating page smoothly using ids

--- Level-3-Task-1-Freelance-Client-Dashboard/src/components/ui/Notification.tsx
+++ Level-3-Task-1-Freelance-Client-Dashboard/src/components/ui/Notification.tsx
@@ -1,11 +1,16 @@
 import { IconCheckbox } from "@tabler/icons-react";
 import useNotification from "../../hooks/useNotification";
+import { HashLink } from "react-router-hash-link";
 
 function Notification({ id }: { id: number }) {
     const { getOne, markAsRead } = useNotification();
     const notification = getOne(id);
     return (
-        <div className="flex w-full items-center justify-between rounded-sm bg-neutral-300 px-1 py-2 ring ring-neutral-800 dark:bg-neutral-700 dark:ring-neutral-200">
+        <HashLink
+            smooth
+            to={`/dashboard/notifications#${id}`}
+            className="flex w-full items-center justify-between rounded-sm bg-neutral-300 px-1 py-2 ring ring-neutral-800 dark:bg-neutral-700 dark:ring-neutral-200"
+        >
             <p className="line-clamp-1 w-[90%] text-xs">
                 {notification?.message}
             </p>
@@ -13,7 +18,7 @@ function Notification({ id }: { id: number }) {
                 className="size-4 cursor-pointer"
                 onClick={() => markAsRead(notification?.id as number)}
             />
-        </div>
+        </HashLink>
     );
 }
 
