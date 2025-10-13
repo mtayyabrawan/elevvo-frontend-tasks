99ffd03e4c088713350e0a7d1838f387da91e0fe
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-10-13 20:31:21 +0500

    completed notifications page

--- Level-3-Task-1-Freelance-Client-Dashboard/src/pages/Notifications.tsx
+++ Level-3-Task-1-Freelance-Client-Dashboard/src/pages/Notifications.tsx
@@ -1,5 +1,36 @@
+import { IconCheckbox, IconTrash } from "@tabler/icons-react";
+import useNotification from "../hooks/useNotification";
+
 function Notifications() {
-    return <div>Notifications</div>;
+    const { notifications, markAsRead, deleteOne } = useNotification();
+    return (
+        <div className="h-full w-full space-y-4 p-4">
+            {notifications.map((notification) => (
+                <div
+                    id={`${notification.id}`}
+                    className="flex h-fit w-full items-center justify-between rounded-sm bg-slate-300 p-4 text-sm ring-[1px] dark:bg-slate-600"
+                >
+                    <p className="w-[70%]">{notification.message}</p>
+                    <div className="flex items-center justify-center gap-4">
+                        {notification.status === "unread" && (
+                            <IconCheckbox
+                                className="size-5 cursor-pointer"
+                                onClick={() =>
+                                    markAsRead(notification?.id as number)
+                                }
+                            />
+                        )}
+                        <IconTrash
+                            className="size-5 cursor-pointer"
+                            onClick={() =>
+                                deleteOne(notification?.id as number)
+                            }
+                        />
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default Notifications;
