aa56e57914f3e5d609401c8e6cb4c01a8a2c7796
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-10-20 00:12:38 +0500

    refactor Login and SignUp components for improved form handling and layout

--- Level-3-Task-1-Freelance-Client-Dashboard/src/pages/Login.tsx
+++ Level-3-Task-1-Freelance-Client-Dashboard/src/pages/Login.tsx
@@ -16,7 +16,7 @@ function Login() {
         handleSubmit,
         reset,
         formState: { errors, isSubmitting },
-    } = useForm({
+    } = useForm<LoginType>({
         resolver: zodResolver(LoginSchema),
         defaultValues: { email: "", password: "" },
     });
@@ -35,16 +35,16 @@ function Login() {
                 onSubmit={handleSubmit(formSubmit)}
                 noValidate
                 autoComplete="off"
-                className="mx-auto mt-10 h-auto w-1/3 space-y-3 rounded-md bg-slate-400/40 px-4 py-8 text-sm shadow-[0px_0px_3px_1px_black]"
+                className="mx-auto mt-10 h-auto w-full max-w-md space-y-3 rounded-md bg-slate-400/40 px-4 py-8 text-sm shadow-[0px_0px_3px_1px_black] sm:max-w-lg md:max-w-xl lg:max-w-[480px]"
             >
                 <h1 className="text-center text-lg font-medium">Login</h1>
                 <div className="relative flex w-full flex-col items-center justify-center gap-2">
                     {errors.email && (
-                        <p className="absolute top-1 right-4 text-xs text-nowrap text-red-500 dark:text-red-400">
+                        <p className="absolute top-1 right-4 text-xs whitespace-nowrap text-red-500 dark:text-red-400">
                             {errors.email.message}
                         </p>
                     )}
-                    <label htmlFor="email" className="w-[93%]">
+                    <label htmlFor="email" className="w-full max-w-[420px]">
                         Email
                     </label>
                     <input
@@ -52,16 +52,16 @@ function Login() {
                         {...register("email")}
                         id="email"
                         autoComplete="off"
-                        className={`w-[95%] rounded-md bg-slate-100 p-2 text-center ring-[1px] focus-visible:outline-hidden dark:bg-slate-950 ${errors.email ? "ring-red-500 dark:ring-red-400" : "ring-slate-500 dark:ring-slate-400"}`}
+                        className={`w-full max-w-[420px] rounded-md bg-slate-100 p-2 text-center ring-[1px] focus-visible:outline-none dark:bg-slate-950 ${errors.email ? "ring-red-500 dark:ring-red-400" : "ring-slate-500 dark:ring-slate-400"}`}
                     />
                 </div>
                 <div className="relative flex w-full flex-col items-center justify-center gap-2">
                     {errors.password && (
-                        <p className="absolute top-1 right-4 text-xs text-nowrap text-red-500 dark:text-red-400">
+                        <p className="absolute top-1 right-4 text-xs whitespace-nowrap text-red-500 dark:text-red-400">
                             {errors.password.message}
                         </p>
                     )}
-                    <label htmlFor="password" className="w-[93%]">
+                    <label htmlFor="password" className="w-full max-w-[420px]">
                         Password
                     </label>
                     <input
@@ -69,19 +69,19 @@ function Login() {
                         {...register("password")}
                         id="password"
                         autoComplete="off"
-                        className={`w-[95%] rounded-md bg-slate-100 p-2 text-center ring-[1px] focus-visible:outline-hidden dark:bg-slate-950 ${errors.password ? "ring-red-500 dark:ring-red-400" : "ring-slate-500 dark:ring-slate-400"}`}
+                        className={`w-full max-w-[420px] rounded-md bg-slate-100 p-2 text-center ring-[1px] focus-visible:outline-none dark:bg-slate-950 ${errors.password ? "ring-red-500 dark:ring-red-400" : "ring-slate-500 dark:ring-slate-400"}`}
                     />
                 </div>
                 <button
                     type="submit"
                     disabled={isSubmitting}
-                    className="transile mx-auto flex w-[95%] cursor-pointer items-center justify-center gap-1 rounded-sm bg-slate-400 p-1.5 hover:bg-slate-400/70 focus-visible:outline-hidden dark:bg-slate-800 dark:hover:bg-slate-800/70"
+                    className="transile mx-auto flex w-full max-w-[420px] cursor-pointer items-center justify-center gap-1 rounded-sm bg-slate-400 p-1.5 hover:bg-slate-400/70 focus-visible:outline-none dark:bg-slate-800 dark:hover:bg-slate-800/70"
                 >
                     <p>Login</p>
                     {isSubmitting ? (
-                        <IconLoaderQuarter className="animate-load size-4" />
+                        <IconLoaderQuarter className="h-4 w-4 animate-spin" />
                     ) : (
-                        <IconLogin className="size-3" />
+                        <IconLogin className="h-4 w-4" />
                     )}
                 </button>
             </form>
--- Level-3-Task-1-Freelance-Client-Dashboard/src/pages/SignUp.tsx
+++ Level-3-Task-1-Freelance-Client-Dashboard/src/pages/SignUp.tsx
@@ -16,7 +16,7 @@ function SignUp() {
         handleSubmit,
         reset,
         formState: { errors, isSubmitting },
-    } = useForm({
+    } = useForm<SignUpType>({
         resolver: zodResolver(SignUpSchema),
         defaultValues: { name: "", email: "", password: "" },
     });
@@ -36,16 +36,16 @@ function SignUp() {
                 onSubmit={handleSubmit(formSubmit)}
                 noValidate
                 autoComplete="off"
-                className="mx-auto mt-10 h-auto w-1/3 space-y-3 rounded-md bg-slate-400/40 px-4 py-8 text-sm shadow-[0px_0px_3px_1px_black]"
+                className="mx-auto mt-10 h-auto w-full max-w-md space-y-3 rounded-md bg-slate-400/40 px-4 py-8 text-sm shadow-[0px_0px_3px_1px_black] sm:max-w-lg md:max-w-xl lg:max-w-[480px]"
             >
                 <h1 className="text-center text-lg font-medium">Sign Up</h1>
                 <div className="relative flex w-full flex-col items-center justify-center gap-2">
                     {errors.name && (
-                        <p className="absolute top-1 right-4 text-xs text-nowrap text-red-500 dark:text-red-400">
+                        <p className="absolute top-1 right-4 text-xs whitespace-nowrap text-red-500 dark:text-red-400">
                             {errors.name.message}
                         </p>
                     )}
-                    <label htmlFor="name" className="w-[93%]">
+                    <label htmlFor="name" className="w-full max-w-[420px]">
                         Name
                     </label>
                     <input
@@ -53,16 +53,16 @@ function SignUp() {
                         {...register("name")}
                         id="name"
                         autoComplete="off"
-                        className={`w-[95%] rounded-md bg-slate-100 p-2 text-center ring-[1px] focus-visible:outline-hidden dark:bg-slate-950 ${errors.name ? "ring-red-500 dark:ring-red-400" : "ring-slate-500 dark:ring-slate-400"}`}
+                        className={`w-full max-w-[420px] rounded-md bg-slate-100 p-2 text-center ring-[1px] focus-visible:outline-none dark:bg-slate-950 ${errors.name ? "ring-red-500 dark:ring-red-400" : "ring-slate-500 dark:ring-slate-400"}`}
                     />
                 </div>
                 <div className="relative flex w-full flex-col items-center justify-center gap-2">
                     {errors.email && (
-                        <p className="absolute top-1 right-4 text-xs text-nowrap text-red-500 dark:text-red-400">
+                        <p className="absolute top-1 right-4 text-xs whitespace-nowrap text-red-500 dark:text-red-400">
                             {errors.email.message}
                         </p>
                     )}
-                    <label htmlFor="email" className="w-[93%]">
+                    <label htmlFor="email" className="w-full max-w-[420px]">
                         Email
                     </label>
                     <input
@@ -70,16 +70,16 @@ function SignUp() {
                         {...register("email")}
                         id="email"
                         autoComplete="off"
-                        className={`w-[95%] rounded-md bg-slate-100 p-2 text-center ring-[1px] focus-visible:outline-hidden dark:bg-slate-950 ${errors.email ? "ring-red-500 dark:ring-red-400" : "ring-slate-500 dark:ring-slate-400"}`}
+                        className={`w-full max-w-[420px] rounded-md bg-slate-100 p-2 text-center ring-[1px] focus-visible:outline-none dark:bg-slate-950 ${errors.email ? "ring-red-500 dark:ring-red-400" : "ring-slate-500 dark:ring-slate-400"}`}
                     />
                 </div>
                 <div className="relative flex w-full flex-col items-center justify-center gap-2">
                     {errors.password && (
-                        <p className="absolute top-1 right-4 text-xs text-nowrap text-red-500 dark:text-red-400">
+                        <p className="absolute top-1 right-4 text-xs whitespace-nowrap text-red-500 dark:text-red-400">
                             {errors.password.message}
                         </p>
                     )}
-                    <label htmlFor="password" className="w-[93%]">
+                    <label htmlFor="password" className="w-full max-w-[420px]">
                         Password
                     </label>
                     <input
@@ -87,17 +87,17 @@ function SignUp() {
                         {...register("password")}
                         id="password"
                         autoComplete="off"
-                        className={`w-[95%] rounded-md bg-slate-100 p-2 text-center ring-[1px] focus-visible:outline-hidden dark:bg-slate-950 ${errors.password ? "ring-red-500 dark:ring-red-400" : "ring-slate-500 dark:ring-slate-400"}`}
+                        className={`w-full max-w-[420px] rounded-md bg-slate-100 p-2 text-center ring-[1px] focus-visible:outline-none dark:bg-slate-950 ${errors.password ? "ring-red-500 dark:ring-red-400" : "ring-slate-500 dark:ring-slate-400"}`}
                     />
                 </div>
                 <button
                     type="submit"
                     disabled={isSubmitting}
-                    className="transile mx-auto flex w-[95%] cursor-pointer items-center justify-center gap-1 rounded-sm bg-slate-400 p-1.5 hover:bg-slate-400/70 focus-visible:outline-hidden dark:bg-slate-800 dark:hover:bg-slate-800/70"
+                    className="transile mx-auto flex w-full max-w-[420px] cursor-pointer items-center justify-center gap-1 rounded-sm bg-slate-400 p-1.5 hover:bg-slate-400/70 focus-visible:outline-none dark:bg-slate-800 dark:hover:bg-slate-800/70"
                 >
                     <p>Sign Up</p>
                     {isSubmitting && (
-                        <IconLoaderQuarter className="animate-load size-4" />
+                        <IconLoaderQuarter className="h-4 w-4 animate-spin" />
                     )}
                 </button>
             </form>
