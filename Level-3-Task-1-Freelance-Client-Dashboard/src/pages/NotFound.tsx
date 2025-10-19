0fbcc80d93ba5d20f8241e110832232bc82b550f
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-10-19 08:33:02 +0500

    copleted not-found page

--- Level-3-Task-1-Freelance-Client-Dashboard/src/pages/NotFound.tsx
+++ Level-3-Task-1-Freelance-Client-Dashboard/src/pages/NotFound.tsx
@@ -1,5 +1,35 @@
+import { IconHome } from "@tabler/icons-react";
+import { useEffect } from "react";
+import { Link } from "react-router";
+
 function NotFound() {
-    return <div>NotFound</div>;
+    useEffect(() => {
+        document.documentElement.classList.toggle(
+            "dark",
+            localStorage.freelance_dashboard_theme === "dark" ||
+                (!("freelance_dashboard_theme" in localStorage) &&
+                    window.matchMedia("(prefers-color-scheme: dark)").matches)
+        );
+    }, []);
+
+    return (
+        <div className="flex h-screen w-screen items-center justify-center bg-slate-300 dark:bg-zinc-800">
+            <div className="flex flex-col items-center justify-center gap-3">
+                <h1 className="text-center text-2xl font-bold text-red-500 dark:text-red-400">
+                    404
+                </h1>
+                <h2 className="text-center text-xl font-medium dark:text-white">
+                    Page not found!
+                </h2>
+                <Link
+                    to="/"
+                    className="flex w-fit items-center justify-center gap-2 rounded-sm bg-zinc-800 px-2 py-1.5 text-xs font-medium text-white dark:bg-slate-300 dark:text-black"
+                >
+                    <IconHome className="size-4" /> <span>Home</span>
+                </Link>
+            </div>
+        </div>
+    );
 }
 
 export default NotFound;
